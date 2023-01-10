8e9c7a6f9c2166fecd214460cb8f8a7ec8076ab4
Osama <osama.0035@gmail.com>
2023-01-10 17:52:41 +0530

    Removed autoplay feature

--- src/pages/Files.jsx
+++ src/pages/Files.jsx
@@ -119,10 +119,8 @@ const Files = () => {
                     <div className="modal">
                       <div className="modal-box relative">
                         <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                        {/* <h3 className="text-lg font-bold">Congratulations random Internet user!</h3> */}
-                        {/* <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p> */}
                         {/* <iframe width="400" height="200" src={items.link} title="yo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-                        <ReactPlayer url={url} playing='true' width='400'/>
+                        <ReactPlayer url={url} controls='true' width='400'/>
                       </div>
                     </div>
                 </label>
